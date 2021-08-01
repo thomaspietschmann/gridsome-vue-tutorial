04f768b2082a55d9791d2eb695208924af7f0ac8
Thomas Pietschmann <thomas@pietschi.com>
2021-08-01 21:59:41 +0200

    adds first stylings

--- e-vents-front/src/layouts/Default.vue
+++ e-vents-front/src/layouts/Default.vue
@@ -2,7 +2,17 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title>Welcome to E-Vents</v-toolbar-title>
-      <v-text-field placeholder="Search" class="ml-8"> </v-text-field>
+      <v-text-field
+        placeholder="Search"
+        class="ml-8"
+        style="max-width: 350px"
+        outlined
+        rounded
+        dense
+        hide-details
+        prepend-inner-icon="mdi-magnify-scan"
+      >
+      </v-text-field>
       <v-spacer></v-spacer>
       <v-btn>Create Event</v-btn>
     </v-app-bar>
